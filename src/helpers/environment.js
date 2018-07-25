39532daca0961fc7de9b5ad10913a61066aa318e
Tyler Shelton <tshelton5000@gmail.com>
2018-07-25 11:30:32 -0400

    what?

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch(window.location.hostname){
         APIURL='http://localhost:3000'
         break;
     case 'tms-recipes-client.herokuapp.com':
-        APIURL = 'https://tms-recipes-server.herokuapp.com/'
+        APIURL = 'https://tms-recipes-server.herokuapp.com'
 }
 
 export default APIURL;
